201a06a0a5904965d86b1b55f8fe576390827f0b
Akash <chatterje1997@gmail.com>
2021-06-23 19:17:36 +0530

    Total done

--- Tic-tac-toe/Tic_tac_toe_game.py
+++ Tic-tac-toe/Tic_tac_toe_game.py
@@ -12,7 +12,17 @@ count=0
 
 #disable all buttons
 def disable_all_buttons():
-	pass
+	b1.config(state=DISABLED)
+	b2.config(state=DISABLED)
+	b3.config(state=DISABLED)
+
+	b4.config(state=DISABLED)
+	b5.config(state=DISABLED)
+	b6.config(state=DISABLED)
+
+	b7.config(state=DISABLED)
+	b8.config(state=DISABLED)
+	b9.config(state=DISABLED)
 
 
 # check if someone wins
@@ -26,6 +36,15 @@ def checkwinner():
 		winner=True
 		messagebox.showinfo("Tic-Tac-Toe-game","Congrats X wins!!!!")
 		disable_all_buttons()
+	
+	elif b4["text"]=="X" and b5["text"]=="X" and b6["text"]=="X":
+		b1.config(bg='red')
+		b4.config(bg='red')
+		b7.config(bg='red')
+		winner=True
+		messagebox.showinfo("Tic-Tac-Toe-game","Congrats X wins!!!!")
+		disable_all_buttons()
+
 	elif b7["text"]=="X" and b8["text"]=="X" and b9["text"]=="X":
 		b7.config(bg='red')
 		b8.config(bg='red')
@@ -40,6 +59,15 @@ def checkwinner():
 		winner=True
 		messagebox.showinfo("Tic-Tac-Toe-game","Congrats X wins!!!!")
 		disable_all_buttons()
+
+	elif b2["text"]=="X" and b5["text"]=="X" and b8["text"]=="X":
+		b1.config(bg='red')
+		b4.config(bg='red')
+		b7.config(bg='red')
+		winner=True
+		messagebox.showinfo("Tic-Tac-Toe-game","Congrats X wins!!!!")
+		disable_all_buttons()
+
 	elif b3["text"]=="X" and b6["text"]=="X" and b9["text"]=="X":
 		b1.config(bg='red')
 		b4.config(bg='red')
@@ -62,6 +90,74 @@ def checkwinner():
 		messagebox.showinfo("Tic-Tac-Toe-game","Congrats X wins!!!!")
 		disable_all_buttons()	
 
+	# check if O wins
+	elif b1["text"]=="O" and b2["text"]=="O" and b3["text"]=="O":
+		b1.config(bg='red')
+		b2.config(bg='red')
+		b3.config(bg='red')
+		winner=True
+		messagebox.showinfo("Tic-Tac-Toe-game","Congrats O wins!!!!")
+		disable_all_buttons()
+	
+	elif b4["text"]=="O" and b5["text"]=="O" and b6["text"]=="O":
+		b1.config(bg='red')
+		b4.config(bg='red')
+		b7.config(bg='red')
+		winner=True
+		messagebox.showinfo("Tic-Tac-Toe-game","Congrats O wins!!!!")
+		disable_all_buttons()
+
+	elif b7["text"]=="O" and b8["text"]=="O" and b9["text"]=="O":
+		b7.config(bg='red')
+		b8.config(bg='red')
+		b9.config(bg='red')
+		winner=True
+		messagebox.showinfo("Tic-Tac-Toe-game","Congrats O wins!!!!")
+		disable_all_buttons()
+	elif b1["text"]=="O" and b4["text"]=="O" and b7["text"]=="O":
+		b1.config(bg='red')
+		b4.config(bg='red')
+		b7.config(bg='red')
+		winner=True
+		messagebox.showinfo("Tic-Tac-Toe-game","Congrats O wins!!!!")
+		disable_all_buttons()
+
+	elif b2["text"]=="O" and b5["text"]=="O" and b8["text"]=="O":
+		b1.config(bg='red')
+		b4.config(bg='red')
+		b7.config(bg='red')
+		winner=True
+		messagebox.showinfo("Tic-Tac-Toe-game","Congrats O wins!!!!")
+		disable_all_buttons()
+
+	elif b3["text"]=="O" and b6["text"]=="O" and b9["text"]=="O":
+		b1.config(bg='red')
+		b4.config(bg='red')
+		b7.config(bg='red')
+		winner=True
+		messagebox.showinfo("Tic-Tac-Toe-game","Congrats X wins!!!!")
+		disable_all_buttons()
+	elif b1["text"]=="O" and b5["text"]=="O" and b9["text"]=="O":
+		b1.config(bg='red')
+		b5.config(bg='red')
+		b9.config(bg='red')
+		winner=True
+		messagebox.showinfo("Tic-Tac-Toe-game","Congrats O wins!!!!")
+		disable_all_buttons()
+	elif b3["text"]=="O" and b5["text"]=="O" and b7["text"]=="O":
+		b3.config(bg='red')
+		b5.config(bg='red')
+		b7.config(bg='red')
+		winner=True
+		messagebox.showinfo("Tic-Tac-Toe-game","Congrats O wins!!!!")
+		disable_all_buttons()
+
+	# check if there tie
+	if count == 9 and winner == False:
+		messagebox.showinfo("Tic-Tac-Toe","It's a tie!!!")
+		disable_all_buttons()
+
+
 #def button clicked function
 def b_click(b):
 	global clicked, count
@@ -69,43 +165,60 @@ def b_click(b):
 		b["text"] = "X"
 		clicked=False
 		count+= 1
+		checkwinner()
 	elif b["text"] == " " and clicked == False:
 		b["text"] = "O"
 		clicked=True
 		count+= 1
+		checkwinner()
 	else:
 		messagebox.showerror("Tic-Tac-Toe","Hey,that box is already been selected.\n Select another box!!!!!")
 
-
-#Buttons creation
-b1 = Button(root,text=" ",font=("Helvetica",20),height=3,width=6,bg=defaultbg,command=lambda:b_click(b1))
-b2 = Button(root,text=" ",font=("Helvetica",20),height=3,width=6,bg=defaultbg,command=lambda:b_click(b2))
-b3 = Button(root,text=" ",font=("Helvetica",20),height=3,width=6,bg=defaultbg,command=lambda:b_click(b3))
-
-
-b4 = Button(root,text=" ",font=("Helvetica",20),height=3,width=6,bg=defaultbg,command=lambda:b_click(b4))
-b5 = Button(root,text=" ",font=("Helvetica",20),height=3,width=6,bg=defaultbg,command=lambda:b_click(b5))
-b6 = Button(root,text=" ",font=("Helvetica",20),height=3,width=6,bg=defaultbg,command=lambda:b_click(b6))
-
-
-b7 = Button(root,text=" ",font=("Helvetica",20),height=3,width=6,bg=defaultbg,command=lambda:b_click(b7))
-b8 = Button(root,text=" ",font=("Helvetica",20),height=3,width=6,bg=defaultbg,command=lambda:b_click(b8))
-b9 = Button(root,text=" ",font=("Helvetica",20),height=3,width=6,bg=defaultbg,command=lambda:b_click(b9))
+# Start the game again
+def reset():
+	global b1,b2,b3,b4,b5,b6,b7,b8,b9
+	global clicked,count
+	clicked=True
+	count = 0
+	#Buttons creation
+	b1 = Button(root,text=" ",font=("Helvetica",20),height=3,width=6,bg=defaultbg,command=lambda:b_click(b1))
+	b2 = Button(root,text=" ",font=("Helvetica",20),height=3,width=6,bg=defaultbg,command=lambda:b_click(b2))
+	b3 = Button(root,text=" ",font=("Helvetica",20),height=3,width=6,bg=defaultbg,command=lambda:b_click(b3))
 
 
-#grid buttons to the screen
-b1.grid(row=0,column=0)
-b2.grid(row=0,column=1)
-b3.grid(row=0,column=2)
-
-b4.grid(row=1,column=0)
-b5.grid(row=1,column=1)
-b6.grid(row=1,column=2)
+	b4 = Button(root,text=" ",font=("Helvetica",20),height=3,width=6,bg=defaultbg,command=lambda:b_click(b4))
+	b5 = Button(root,text=" ",font=("Helvetica",20),height=3,width=6,bg=defaultbg,command=lambda:b_click(b5))
+	b6 = Button(root,text=" ",font=("Helvetica",20),height=3,width=6,bg=defaultbg,command=lambda:b_click(b6))
 
 
-b7.grid(row=2,column=0)
-b8.grid(row=2,column=1)
-b9.grid(row=2,column=2)
+	b7 = Button(root,text=" ",font=("Helvetica",20),height=3,width=6,bg=defaultbg,command=lambda:b_click(b7))
+	b8 = Button(root,text=" ",font=("Helvetica",20),height=3,width=6,bg=defaultbg,command=lambda:b_click(b8))
+	b9 = Button(root,text=" ",font=("Helvetica",20),height=3,width=6,bg=defaultbg,command=lambda:b_click(b9))
 
+
+	#grid buttons to the screen
+	b1.grid(row=0,column=0)
+	b2.grid(row=0,column=1)
+	b3.grid(row=0,column=2)
+
+	b4.grid(row=1,column=0)
+	b5.grid(row=1,column=1)
+	b6.grid(row=1,column=2)
+
+
+	b7.grid(row=2,column=0)
+	b8.grid(row=2,column=1)
+	b9.grid(row=2,column=2)
+
+#create restart menu
+my_menu = Menu(root)
+root.config(menu=my_menu)
+
+#create option menu
+option_menu = Menu(my_menu,tearoff=False)
+my_menu.add_cascade(label="options",menu=option_menu)
+option_menu.add_command(label="Restart",command=reset)
+
+reset()
 
 mainloop()
